fcbcb250951cec78a3da9f432cec391fa2cf8e86
Pere Urbon <pere@confluent.io>
2022-01-29 09:58:00 +0100

    add code to handle case when b is bigger than a

--- src/main/java/com/purbon/mac/Subtraction.java
+++ src/main/java/com/purbon/mac/Subtraction.java
@@ -6,7 +6,24 @@ public class Subtraction {
 
     }
 
-    public static int minus(int[] a, int[] b) {
+    public int minus(int[] a, int [] b) {
+        int ai = asInt(a);
+        int bi = asInt(b);
+        if (bi > ai) {
+            return doMinus(b, a) * -1;
+        }
+        return doMinus(a, b);
+    }
+
+    private int asInt(int[] a) {
+        int r = 0;
+        for(int i=a.length-1; i>=0; i--) {
+            r += a[i]*(int)Math.pow(10, a.length-1-i);
+        }
+        return r;
+    }
+
+    private int doMinus(int[] a, int[] b) {
 
         if (a.length != b.length) {
             throw new IllegalArgumentException("A should be of same length than B");
--- src/test/java/com/purbon/mac/SubtractionTest.java
+++ src/test/java/com/purbon/mac/SubtractionTest.java
@@ -7,16 +7,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SubtractionTest {
 
+    Subtraction s;
+
     @BeforeEach
     public void before() {
-
+        s = new Subtraction();
     }
 
     @Test
     public void subtractionOfNumbersShouldGiveCorrectResult() {
         var a = new int[]{4, 7};
         var b = new int[]{1, 5};
-        var r = Subtraction.minus(a, b);
+        var r = s.minus(a, b);
         assertThat(r).isEqualTo(32);
     }
 
@@ -24,7 +26,7 @@ public class SubtractionTest {
     public void subtractionOfCarryOnNumbersShouldGiveCorrectResult() {
         var a = new int[]{4, 0};
         var b = new int[]{1, 5};
-        var r = Subtraction.minus(a, b);
+        var r = s.minus(a, b);
         assertThat(r).isEqualTo(25);
     }
 
@@ -32,7 +34,15 @@ public class SubtractionTest {
     public void subtractionOfLongNumbersShouldGiveCorrectResult() {
         var a = new int[]{3, 4, 0};
         var b = new int[]{2, 1, 5};
-        var r = Subtraction.minus(a, b);
+        var r = s.minus(a, b);
         assertThat(r).isEqualTo(125);
+    }
+
+    @Test
+    public void subtractionOfOutOfOrderNumbersShouldGiveCorrectResult() {
+        var a = new int[]{1, 5};
+        var b = new int[]{4, 0};
+        var r = s.minus(a, b);
+        assertThat(r).isEqualTo(-25);
     }
 }
